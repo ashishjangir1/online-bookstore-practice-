99aaf6fac123c08b647b0d8ef500a8af7e089804
Ashish Jangir <ashish.jangir@financialforce.com>
2020-06-20 23:54:27 +0530

    working till displaying table from getting rest data from server

--- angular-bookstore/src/app/app.module.ts
+++ angular-bookstore/src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BookListComponent } from './components/book-list/book-list.component';
+import { BookService } from './service/book.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [BookService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
